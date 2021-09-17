d48185ca3b358bff97c6733923fa41999f347987
罗 贵丰 <luoguifeng@cmiot.chinamobile.com>
2021-09-17 18:28:49 +0800

    Update: 更细

--- app/api/user.js
+++ app/api/user.js
@@ -1,8 +1,21 @@
 const model = require('../model');
+const Validate = require('request-validate')
 const config = require('../../config.default');
 
 class User {
+    registerRule() {
+        return {
+            rule: {
+                openId: 'required',
+
+            },
+            message: {
+                "openId.required": "openId参数不能为空"
+            }
+        }
+    }
     async register(ctx, next) {
+        Validate(ctx.request.body, this.registerRule().rule, this.register.message)
         let { account, password } = ctx.request.body;
         ctx.body = {
             "errno": 0,
