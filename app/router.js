e8247f83ad3d2d45d5bbe3218ed46ff7934ef56f
luogf <zoroshow@outlook.com>
2021-09-27 23:36:08 +0800

    Update: 新增方法前端传入wxcode

--- app/router.js
+++ app/router.js
@@ -5,5 +5,6 @@ const routerApi = new Router();
 console.log(api.User.register);
 routerApi.post('/api/register', api.User.register);
 routerApi.post('/api/signIn', api.User.signIn);
+routerApi.post('/api/wx/code', api.User.wxCode);
 
 module.exports = routerApi;
