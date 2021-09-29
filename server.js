87fc94cf9034c977b02e4fcf0182b4c50b36acd6
罗 贵丰 <luoguifeng@cmiot.chinamobile.com>
2021-09-29 16:50:18 +0800

    Update：完成xmlbody解析

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ global.config = config
 
 const Koa = require('koa');
 const cors = require('koa2-cors');
+const xmlParser = require('koa-xml-body')
 const bodyParser = require('koa-bodyparser');
 const moment = require('moment');
 const router = require('./app/router');
 
 const app = new Koa();
 app.use(cors());
+app.use(xmlParser())
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
