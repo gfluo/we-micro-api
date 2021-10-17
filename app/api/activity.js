f58bf14cd007cc4c7f3ee590c8465539e37be70a
luogf <zoroshow@outlook.com>
2021-10-17 19:14:07 +0800

    Update: 整理数据 bug fix2

--- app/api/activity.js
+++ app/api/activity.js
@@ -9,7 +9,7 @@ class Activity {
         activityAll.rows = activityAll.rows.map((item => {
             item = item.toJSON();
             let period = item.endTime > item.startTime ? item.endTime - item.startTime : 0;
-            item.period = Math.ceil(period / 3600 * 1000) + '小时';
+            item.period = Math.ceil(period / (3600 * 1000)) + '小时';
             item.startTime = moment(item.startTime).format("YYYY-MM-DD HH:mm:ss");
             item.amount = item.amount / 100;
             return item;
