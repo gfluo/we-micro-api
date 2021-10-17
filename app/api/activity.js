137e7b2cf4662ad9a9752b636befd9f0e49dfc13
luogf <zoroshow@outlook.com>
2021-10-17 18:56:00 +0800

    Update: 整理数据

--- app/api/activity.js
+++ app/api/activity.js
@@ -1,10 +1,17 @@
 const model = require('../model');
+const moment = require('moment');
 
 class Activity {
     activities = async (ctx, next) => {
         let activityAll = await model.Activity.findAndCountAll({
 
         }) 
+        activityAll.rows = activityAll.rows.map((item => {
+            item = item.toJSON();
+            let period = item.endTime > item.startTime ? item.endTime - item.startTime : 0;
+            item.period = Math.ceil(period / 3600) + '小时';
+            item.startTime = moment(item.startTime).format("YYYY-MM-DD HH:mm:ss");
+        }))
         ctx.body = {
             errno: 0,
             error: "",
