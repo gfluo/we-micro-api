eb6f0ef5a4fc961a30360300d11d239a67dcb9af
luogf <zoroshow@outlook.com>
2022-03-27 13:27:48 +0800

    Add: micro second

--- config.default.js
+++ config.default.js
@@ -1,8 +1,8 @@
 module.exports = {
-    listenPort: 4000,
+    listenPort: 4001,
     db: {
         host: "123.60.8.4",
-        database: 'wx_micro',
+        database: 'wx_micro-second',
         username: 'root',
         password: 'luogf123',
         port: 3306,
