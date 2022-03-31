bd274aceeff4044c045b4e77dc76a1ff99e38449
luogf <zoroshow@outlook.com>
2022-03-31 22:56:48 +0800

    Update: 完成凤飞阁楼配置替换

--- config.default.js
+++ config.default.js
@@ -8,10 +8,16 @@ module.exports = {
         port: 3306,
     },
     wxMicro: {
-        appKey: 'wx4bfe6b7ee9a89f2c',
-        bussiessId: '1613748468',
-        appSecret: 'ca4c0aa72e75f9fbf7d27337c948bb45',
+        // appKey: 'wx4bfe6b7ee9a89f2c',
+        // bussiessId: '1613748468',
+        // appSecret: 'ca4c0aa72e75f9fbf7d27337c948bb45',
+        // apiSecret: 'QWERTYUIOP987654321asdfghjkl1234',
+        // 凤飞阁
+        appKey: 'wx33d2867c2d783db1',
+        bussinessId: '1623417166',
+        appSecret: '12a9913992f2cd11523032183f96c3ac',
         apiSecret: 'QWERTYUIOP987654321asdfghjkl1234',
+
     },
     domain: 'https://www.szstswh.com'
 }
