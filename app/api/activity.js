d3be3384102c0f219495d4eddf9ae1ebb8218b32
zoroluo <zoroshow@outlook.com>
2025-11-10 17:53:32 +0800

    Fix: pagesize not number 4

--- app/api/activity.js
+++ app/api/activity.js
@@ -8,6 +8,7 @@ class Activity {
     getActivities = async (ctx, next) => {
         //新增查询功能
         let { query, page, pagesize } = ctx.query;
+        console.log(ctx.query);
         console.log('-------------------------------------------------------------------------------', query, page, pagesize);
         if (!page || !util.isInteger(page) || !pagesize || !util.isInteger(pagesize)) {
             ctx.body = {
@@ -17,6 +18,8 @@ class Activity {
                     activities: []
                 }
             }
+
+            return next();
         }
 
         page = parseInt(page);
